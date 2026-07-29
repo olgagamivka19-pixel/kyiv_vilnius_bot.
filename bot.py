a51e67c63318ca976851bb86b8baf7b21719d789
olgagamivka19-pixel <olgagamivka19@gmail.com>
2026-07-29 12:47:54 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -73,16 +73,43 @@ async def start(message: types.Message):
 @dp.callback_query()
 
 async def language(callback: types.CallbackQuery):
+if callback.data == "uk":
 
-    if callback.data == "uk":
+    keyboard = InlineKeyboardMarkup(
 
-        await callback.message.answer(
+        inline_keyboard=[
 
-            "Чи сподобався вам візит?\n\n"
+            [
 
-            "Оберіть оцінку ⭐️"
+                InlineKeyboardButton(text="⭐️ 1", callback_data="rating_1"),
 
-        )
+                InlineKeyboardButton(text="⭐️ 2", callback_data="rating_2"),
+
+                InlineKeyboardButton(text="⭐️ 3", callback_data="rating_3")
+
+            ],
+
+            [
+
+                InlineKeyboardButton(text="⭐️ 4", callback_data="rating_4"),
+
+                InlineKeyboardButton(text="⭐️ 5", callback_data="rating_5")
+
+            ]
+
+        ]
+
+    )
+
+    await callback.message.answer(
+
+        "Чи сподобався вам візит?\n\n"
+
+        "Оберіть оцінку ⭐️",
+
+        reply_markup=keyboard
+
+    )
 
     await callback.answer()
 
